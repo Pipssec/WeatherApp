1a261d07eaa2018e137503fe1a868fc82c52e49d
Artsem Stsepanenka <asstep@mail.ru>
2023-01-18 17:15:30 +0300

    Add ListItem

--- app/src/main/java/com/example/weatherapp/screens/MainScreen.kt
+++ app/src/main/java/com/example/weatherapp/screens/MainScreen.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp.screens
 
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -154,7 +155,13 @@ fun TabLayout() {
          state = pagerState,
          modifier = Modifier.weight(1.0f)
      ) {index ->
-
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ){
+            items(count = 15){
+                ListItem()
+            }
+        }
          
      }
     }
