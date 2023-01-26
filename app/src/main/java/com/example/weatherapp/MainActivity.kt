9b26da27c0d1e59396354a48bb255dc54c93c9ca
Artsem Stsepanenka <asstep@mail.ru>
2023-01-26 12:38:37 +0300

    Formated Project

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,18 +3,7 @@ package com.example.weatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import com.example.weatherapp.data.WeatherModel
-import com.example.weatherapp.screens.MainCard
-import com.example.weatherapp.screens.TabLayout
+import com.example.weatherapp.screens.StartScreen
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,34 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppTheme {
-                val daysList = remember {
-                    mutableStateOf(listOf<WeatherModel>())
-                }
-                val currentDay = remember {
-                    mutableStateOf(WeatherModel(
-                        "",
-                        "",
-                        "0",
-                        "",
-                        "",
-                        "0",
-                        "0",
-                        "",
-                    ))
-                }
-                getData("Gomel", this, daysList, currentDay)
-                Image(
-                    painter = painterResource(id = R.drawable.weatherapp),
-                    contentDescription = "im1",
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .alpha(0.5f),
-                    contentScale = ContentScale.FillBounds,
-                )
-                Column {
-                    MainCard(currentDay)
-                    TabLayout(daysList, currentDay)
-                }
+                StartScreen(this)
             }
         }
     }
---
+++ app/src/main/java/com/example/weatherapp/screens/StartScreen.kt
@@ -0,0 +1,50 @@
+package com.example.weatherapp.screens
+
+import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.weatherapp.R
+import com.example.weatherapp.data.WeatherModel
+import com.example.weatherapp.getData
+
+@Composable
+fun StartScreen(context: Context){
+    val daysList = remember {
+        mutableStateOf(listOf<WeatherModel>())
+    }
+    val currentDay = remember {
+        mutableStateOf(
+            WeatherModel(
+            "",
+            "",
+            "0",
+            "",
+            "",
+            "0",
+            "0",
+            "",
+        )
+        )
+    }
+    getData("Gomel", context, daysList, currentDay)
+    Image(
+        painter = painterResource(id = R.drawable.weatherapp),
+        contentDescription = "im1",
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(0.5f),
+        contentScale = ContentScale.FillBounds,
+    )
+    Column {
+        MainCard(currentDay)
+        TabLayout(daysList, currentDay)
+    }
+}
